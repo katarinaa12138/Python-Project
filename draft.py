dc759e16d412fc14108d0f14ce0bf47494ceba5d
angelawong123 <cwong3@babson.edu>
2023-04-06 17:38:21 -0400

    edit draft

--- draft.py
+++ draft.py
@@ -34,26 +34,70 @@ extensions = {
     ".ppt": "PowerPoint Files",
     ".csv": "CSV Files",
     ".r": "R Files",
-    ".py": "Python Files"
+    ".py": "Python Files",
 }
 
-# Define a function to organize the files in a folder
-def organize_files(folder_path):
-    # Move all files in subdirectories to folder_path
+# # Define a function to organize the files in a folder
+# def organize_files(folder_path):
+#     # Move all files in subdirectories to folder_path
+#     for dirpath, dirnames, filenames in os.walk(folder_path):
+#         for filename in filenames:
+#             src_path = os.path.join(dirpath, filename)
+#             dest_path = os.path.join(folder_path, filename)
+#             os.rename(src_path, dest_path)
+
+#     # Delete all empty subdirectories in folder_path
+#     for dirpath, dirnames, filenames in os.walk(folder_path, topdown=False):
+#         for dirname in dirnames:
+#             dir_to_remove = os.path.join(dirpath, dirname)
+#             if not os.listdir(dir_to_remove):
+#                 os.rmdir(dir_to_remove)
+
+#     # Organize all the files in the folder
+#     for file_name in os.listdir(folder_path):
+#         # Get the file extension
+#         file_extension = os.path.splitext(file_name)[1].lower()
+
+#         # Get the destination directory for this file
+#         if file_extension in extensions:
+#             dest_dir = os.path.join(folder_path, extensions[file_extension])
+#         else:
+#             dest_dir = os.path.join(folder_path, "Other")
+
+#         # Create the destination directory if it doesn't exist
+#         os.makedirs(dest_dir, exist_ok=True)
+
+#         # Move the file to the destination directory
+#         src_path = os.path.join(folder_path, file_name)
+#         dest_path = os.path.join(dest_dir, file_name)
+#         os.rename(src_path, dest_path)
+
+def move_files(folder_path):
+    """
+    This function moves all files in subdirectories to folder_path
+    """
     for dirpath, dirnames, filenames in os.walk(folder_path):
         for filename in filenames:
             src_path = os.path.join(dirpath, filename)
             dest_path = os.path.join(folder_path, filename)
             os.rename(src_path, dest_path)
 
-    # Delete all empty subdirectories in folder_path
+
+def delete_subdirectories(folder_path):
+    """
+    This function deletes all empty subdirectories in folder_path.
+    """
     for dirpath, dirnames, filenames in os.walk(folder_path, topdown=False):
         for dirname in dirnames:
             dir_to_remove = os.path.join(dirpath, dirname)
             if not os.listdir(dir_to_remove):
                 os.rmdir(dir_to_remove)
-    
-    # Organize all the files in the folder
+
+
+def organize_files_in_folder(folder_path):
+    """
+    This function organizes all the files in the folder according to file extensions.
+    """
     for file_name in os.listdir(folder_path):
         # Get the file extension
         file_extension = os.path.splitext(file_name)[1].lower()
@@ -72,7 +116,19 @@ def organize_files(folder_path):
         dest_path = os.path.join(dest_dir, file_name)
         os.rename(src_path, dest_path)
 
+
+def organize_files(folder_path):
+    """
+    This function combines all the previous functions and organizes the files.
+    """
+    print("Sorting files.....")
+    move_files(folder_path)
+    delete_subdirectories(folder_path)
+    organize_files_in_folder(folder_path)
+    print("Sorting Completed")
+
+
 # Example usage:
 if __name__ == "__main__":
-    folder_path = "C:/Users/xwang2/Documents/temp"
+    folder_path = "C:/Users/cwong3/Documents/temp"
     organize_files(folder_path)
