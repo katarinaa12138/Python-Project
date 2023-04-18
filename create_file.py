4b251af4e0ced0e8ef609d59ef37a441e5a53359
katarinaa12138 <xwang2@babson.edu>
2023-04-18 18:46:40 -0400

    update

--- create_file.py
+++ create_file.py
@@ -7,30 +7,30 @@ import string
 def generate_random_string(length):
     return ''.join(random.choices(string.ascii_letters, k=length))
 
-# Mapping of file extensions to purpose-based names
+# Mapping of file extensions to their respective extensions
 file_extensions = {
-    'pdf': {'name': 'life', 'ext': '.pdf'},
-    'word': {'name': 'entertainment', 'ext': '.docx'},
-    'excel': {'name': 'study', 'ext': '.xlsx'},
-    'video': {'name': 'life', 'ext': '.mp4'},
-    'python': {'name': 'study', 'ext': '.py'},
-    'picture': {'name': 'entertainment', 'ext': '.jpg'},
-    'r_codes': {'name': 'study', 'ext': '.R'},
-    'mpg': {'name': 'entertainment', 'ext': '.mpg'},
-    'tar': {'name': 'study', 'ext': '.tar'},
-    'music': {'name': 'entertainment', 'ext': '.mp3'},
-    'text': {'name': 'study', 'ext': '.txt'},
-    'csv': {'name': 'study', 'ext': '.csv'},
-    'html': {'name': 'entertainment', 'ext': '.html'},
-    'css': {'name': 'entertainment', 'ext': '.css'},
-    'javascript': {'name': 'study', 'ext': '.js'},
-    'xml': {'name': 'study', 'ext': '.xml'},
-    'json': {'name': 'study', 'ext': '.json'},
-    'powerpoint': {'name': 'entertainment', 'ext': '.pptx'},
-    'database': {'name': 'study', 'ext': '.db'},
-    'compressed': {'name': 'study', 'ext': '.zip'},
-    'executable': {'name': 'study', 'ext': '.exe'},
-    'markdown': {'name': 'study', 'ext': '.md'}
+    'pdf': '.pdf',
+    'docx': '.docx',
+    'xlsx': '.xlsx',
+    'mp4': '.mp4',
+    'py': '.py',
+    'jpg': '.jpg',
+    'R': '.R',
+    'mpg': '.mpg',
+    'tar': '.tar',
+    'mp3': '.mp3',
+    'txt': '.txt',
+    'csv': '.csv',
+    'html': '.html',
+    'css': '.css',
+    'js': '.js',
+    'xml': '.xml',
+    'json': '.json',
+    'pptx': '.pptx',
+    'db': '.db',
+    'zip': '.zip',
+    'exe': '.exe',
+    'md': '.md'
 }
 
 # Desktop path
@@ -42,10 +42,11 @@ folder_path = os.path.join(desktop_path, folder_name)
 os.makedirs(folder_path, exist_ok=True)
 
 # Generate sample files in the folder
-for file_type, file_info in file_extensions.items():
-    file_name = file_info['name'] + '_' + generate_random_string(10) + file_info['ext']
-    file_path = os.path.join(folder_path, file_name)
-    with open(file_path, 'wb') as file:
-        file.write(os.urandom(1024))  # Write random data to file
+for file_extension, extension in file_extensions.items():
+    for i in range(10): # create 5 files for each extension
+        file_name = generate_random_string(10) + extension
+        file_path = os.path.join(folder_path, file_name)
+        with open(file_path, 'wb') as file:
+            file.write(os.urandom(1024))  # Write random data to file
 
 print("Sample files created successfully in folder: ", folder_path)
