adfb153916c3a131bcc8ff33fdd454c96e41f0d3
katarinaa12138 <xwang2@babson.edu>
2023-04-18 18:56:52 -0400

    update

--- create_file.py
+++ create_file.py
@@ -2,36 +2,17 @@ import os
 import shutil
 import random
 import string
+from faker import Faker
+from datetime import datetime, timedelta
 
 # Function to generate random string
 def generate_random_string(length):
     return ''.join(random.choices(string.ascii_letters, k=length))
 
-# Mapping of file extensions to their respective extensions
-file_extensions = {
-    'pdf': '.pdf',
-    'docx': '.docx',
-    'xlsx': '.xlsx',
-    'mp4': '.mp4',
-    'py': '.py',
-    'jpg': '.jpg',
-    'R': '.R',
-    'mpg': '.mpg',
-    'tar': '.tar',
-    'mp3': '.mp3',
-    'txt': '.txt',
-    'csv': '.csv',
-    'html': '.html',
-    'css': '.css',
-    'js': '.js',
-    'xml': '.xml',
-    'json': '.json',
-    'pptx': '.pptx',
-    'db': '.db',
-    'zip': '.zip',
-    'exe': '.exe',
-    'md': '.md'
-}
+# Function to generate random date in the past
+def generate_random_date():
+    days_ago = random.randint(1, 1000)
+    return datetime.now() - timedelta(days=days_ago)
 
 # Desktop path
 desktop_path = os.path.expanduser("~/Desktop")
@@ -42,11 +23,16 @@ folder_path = os.path.join(desktop_path, folder_name)
 os.makedirs(folder_path, exist_ok=True)
 
 # Generate sample files in the folder
+fake = Faker()
 for file_extension, extension in file_extensions.items():
-    for i in range(10): # create 5 files for each extension
-        file_name = generate_random_string(10) + extension
+    for i in range(5): # create 5 files for each extension
+        fake_name = fake.file_name(extension=extension)
+        file_name = generate_random_string(10) + "_" + fake_name
         file_path = os.path.join(folder_path, file_name)
+        file_size = random.randint(1024, 1048576000) # generate file size between 1KB to 1GB
         with open(file_path, 'wb') as file:
-            file.write(os.urandom(1024))  # Write random data to file
+            file.write(os.urandom(file_size))  # Write random data to file
+        creation_time = generate_random_date().timestamp() # generate random timestamp in the past
+        os.utime(file_path, (creation_time, creation_time)) # set file creation time
 
 print("Sample files created successfully in folder: ", folder_path)
